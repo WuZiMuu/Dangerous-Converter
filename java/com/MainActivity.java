7839cdc0ecdfd750099efc73c5147b2c7b8566ce
WuZiMuu <wuzimuuu@gmail.com>
2016-01-01 21:59:11 +0100

    Create MainActivity.java

---
+++ java/com/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.bmw.dangerousconverter;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final EditText editMiles = (EditText) findViewById(R.id.editMiles);
+
+        final EditText editYards = (EditText) findViewById(R.id.editYards);
+
+
+
+        Button buttonConvert = (Button) findViewById(R.id.buttonConvert);
+
+       buttonConvert.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View arg0) {
+
+               double miles = Double.valueOf(editMiles.getText().toString());
+               double yards = miles/0.000493736501;
+               editYards.setText(String.valueOf(yards));
+
+           }
+       });
+    ;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, About.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
